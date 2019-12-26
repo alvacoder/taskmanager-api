584a0b97770bab23941a774c183549a73fa2ef4b
realprinceviper <idrisadeniji01@gmail.com>
2019-12-26 12:04:56 +0100

    Create endpoint for new user creation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+require('./db/mongoose');
+const User = require('./models/User');
 
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res)=> {
+    const user = req.body;
+
+    User.save(user).then(()=> {
+        res.send(user);
+    }).catch((error) => {
+        res.send(error);
+    })
+})
+
 
 app.listen(port, ()=> {
     console.log('Server is up on port ', port);
