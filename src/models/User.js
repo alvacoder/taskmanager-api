7d945c26e3761f6a61de84243ec8554587034aff
realprinceviper <idrisadeniji01@gmail.com>
2019-12-26 11:57:43 +0100

    create user model

---
+++ src/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('../db/mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email address is invalid');
+            }
+        }
+    },
+    
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number.')
+            }
+        }
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('contains string \'password\'');
+            }
+        }
+    }
+})
