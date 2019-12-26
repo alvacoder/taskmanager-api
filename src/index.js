4f73022e5a6d1e60818e25ff963416a37c5e02d0
realprinceviper <idrisadeniji01@gmail.com>
2019-12-26 13:55:29 +0100

    use appropriate status code for successful creation of data

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.post('/users', (req, res)=> {
     const user = new User(req.body);
 
     user.save().then(()=> {
-        res.send(user);
+        res.status(201).send(user);
     }).catch((error) => {
         res.status(400).send(error);
     })
@@ -21,7 +21,7 @@ app.post('/users', (req, res)=> {
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
     task.save().then(() => {
-        res.send(task);
+        res.status(201).send(task);
     }).catch((error) => {
         res.status(400).send(error);
     })
