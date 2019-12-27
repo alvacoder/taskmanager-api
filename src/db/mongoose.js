6f569d0c003e98c9447541a50d89ed45fb3c7cf0
realprinceviper <idrisadeniji01@gmail.com>
2019-12-27 04:58:51 +0100

    prevent use of deprecated queries

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/task-manager-api', {
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useFindAndModify: false
 })
