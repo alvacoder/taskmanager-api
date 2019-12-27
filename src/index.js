7881ec4531f14bdedd17051a7f68073c711ee087
realprinceviper <idrisadeniji01@gmail.com>
2019-12-27 04:59:41 +0100

    restructure endpoints to use async methods

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get('/users', (req, res) => {
+//using call backs
+
+/* app.get('/users', (req, res) => {
     User.find({}).then((user) => {
         res.send(user);
     }).catch((error)=> {
@@ -67,6 +69,78 @@ app.post('/tasks', (req, res) => {
     }).catch((error) => {
         res.status(400).send(error);
     })
+})
+
+*/
+
+
+//Using Async Await
+
+app.get('/users', async (req, res) => {
+    //const user = new User;
+    try {
+        const user = await User.find({})
+        res.send(user);
+
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
+app.get('/users/:id', async(req, res)=> {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if(!user) {
+            return res.status(404);
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
+app.post('/users', async(req, res)=> {
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send();
+    }
+})
+
+app.get('/tasks', async(req, res)=> {
+    try {
+        const task = await Task.find({})
+        res.send(task)
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
+app.get('/tasks/:id', async(req, res)=> {
+    const _id = req.params.id
+    try {
+        const task = await Task.findById(_id)
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+app.post('/tasks', async(req, res)=> {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.send(task)
+    } catch (error) {
+        res.status(400).send();
+    }
 })
 
 
